cf974e971e54614049ff5652e1c998f151e689c8
Liuri Loami <liuriloami@gmail.com>
2016-09-22 12:57:03 -0300

    Supress tests console logs

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,3 @@
 cd tests
 npm install
-./node_modules/jasmine-node/bin/jasmine-node . --junitreport --output ../build/tests
+./node_modules/jasmine-node/bin/jasmine-node . --junitreport --output ../build/tests >> /dev/null
