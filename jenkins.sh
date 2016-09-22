330857d72b694fe9ebe31d2fc3384b85faad857e
Liuri Loami <liuriloami@gmail.com>
2016-09-22 13:01:05 -0300

    Trying to supress shell error messages

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,3 @@
 cd tests
 npm install
-./node_modules/jasmine-node/bin/jasmine-node . --junitreport --output ../build/tests >> /dev/null
+./node_modules/jasmine-node/bin/jasmine-node . --junitreport --output ../build/tests 2> /dev/null
