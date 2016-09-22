3416a1d3740c34e91d0aaeccd76395871d7729b5
Liuri Loami <liuriloami@gmail.com>
2016-09-22 12:07:07 -0300

    Testing structured tests

--- tests/efetua-login.spec.js
+++ tests/efetua-login.spec.js
@@ -1,16 +1,30 @@
 var frisby = require('frisby');
 var URL_USER = 'http://btmgm.tokenlab.com.br/api'
 
-frisby.create('Testa login')
-.post(URL_USER + '/auth/web'
-  ,{ username: 'validador' , password: 'validador' }
-  ,{ json: true }
-  ,{ headers: { 'Content-Type': 'application/json' }})
-.expectStatus(200)
-.expectHeaderContains('content-type', 'application/json')
-.inspectBody()
-.inspectRequest()
-.expectJSONTypes({
-  token: String,
-  user: {type: String}
-}).toss();
+describe("Test login service", function() {
+  
+  it("should login successfully", function() {
+    frisby.create('do login')
+    .post(URL_USER + '/auth/web', {
+      username: 'validador',
+      password: 'validador'
+    })
+    .expectStatus(200)
+    .expectJSONTypes({
+      token: String,
+      user: {type: String}
+    })
+    .toss();
+  });
+    
+  it("should return an authentication error", function() {
+    frisby.create('do login')
+    .post(URL_USER + '/auth/web', {
+      username: 'validador',
+      password: 'validador2'
+    })
+    .expectStatus(401)
+    .toss();
+  });
+  
+});
--- tests/lista-academias.spec.js
+++ tests/lista-academias.spec.js
@@ -1,25 +1,60 @@
 var frisby = require('frisby');
 var URL_USER = 'http://btmgm.tokenlab.com.br/api'
 
-frisby.create('Testa listagem de academias detalhada: Efetua login')
-.post(URL_USER + '/auth/web'
-  ,{ username: 'validador' , password: 'validador' }
-  ,{ json: true }
-  ,{ headers: { 'Content-Type': 'application/json' }})
-.expectStatus(200)
-.expectHeaderContains('content-type', 'application/json')
-.afterJSON(function (res) {
-  frisby.globalSetup({
-    request: {
-      headers: { 'Authorization': res.token }
-      }
+describe("Listing detailed gyms", function() {
+  it("should return a list of gyms", function() {
+    function login() {
+      frisby.create('Do login')
+      .post(URL_USER + '/auth/web', {
+        username: 'admin',
+        password: 'admin'
+      })
+      .expectStatus(200)
+      .afterJSON(listDetailedGyms)
+      .toss()
+    }
+    function listDetailedGyms(res) {
+      frisby.create('Return list of detailed gyms')
+      .get(URL_USER + '/gym/details', {
+        headers: { 'Authorization': res.token }
+      })
+      .expectStatus(200)
+      .expectJSONTypes({
+        gyms: Array,
+        days_to_expire: Number,
+        start_date: String,
+        end_date: String
+      })
+      .toss();
+    }
+    login();
   });
-  frisby.create('Testa listagem de academias detalhada: Lista detalhe das academias')
-   .get(URL_USER + '/gym')
-   .expectStatus(200)
-   .expectHeaderContains('content-type', 'application/json')
-   .expectJSONTypes({
-     id: undefined,
-     name: undefined,
-   }).toss();
-}).toss();
+
+  it("should return an empty list of gyms", function() {
+    function login() {
+      frisby.create('Do login')
+      .post(URL_USER + '/auth/web', {
+        username: 'admin',
+        password: 'admin'
+      })
+      .expectStatus(200)
+      .afterJSON(listDetailedGyms)
+      .toss()
+    }
+    function listDetailedGyms(res) {
+      frisby.create('Return list of detailed gyms')
+      .get(URL_USER + '/gym/details?gym=99999', {
+        headers: { 'Authorization': res.token }
+      })
+      .expectStatus(200)
+      .expectJSONTypes({
+        gyms: Array,
+        days_to_expire: Number,
+        start_date: String,
+        end_date: String
+      })
+      .toss();
+    }
+    login();
+  });
+});
